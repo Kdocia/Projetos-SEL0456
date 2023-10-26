6c7441e46e0976442db4dbb19b77ce2edb67a74c
Kdocia <134646850+Kdocia@users.noreply.github.com>
2023-10-26 15:03:52 -0300

    Inicio

--- Fibonacci/fibonacci.py
+++ Fibonacci/fibonacci.py
@@ -1,1 +1,30 @@
-class 
+class Fibonacci:
+    def __init__(self, n):
+        self.n = n
+        self.sequence = []
+
+    def generate_sequence(self):
+        a, b = 0, 1
+        for _ in range(self.n):
+            self.sequence.append(a)
+            a, b = b, a + b
+
+    def get_sequence(self):
+        return self.sequence
+
+    def display_sequence(self):
+        print("Sequência de Fibonacci:")
+        for number in self.sequence:
+            print(number, end=" ")
+        print()
+
+
+# Crie uma instância da classe Fibonacci para gerar a sequência
+n = int(input("Quantos números da sequência de Fibonacci você deseja gerar? "))
+fib = Fibonacci(n)
+
+# Gere a sequência de Fibonacci
+fib.generate_sequence()
+
+# Exiba a sequência gerada
+fib.display_sequence()
